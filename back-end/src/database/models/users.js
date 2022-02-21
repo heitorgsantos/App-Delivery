a88ee229e522185365587d3f0d7ab52fac317a72
Heitor Santos <heitorg33@gmail.com>
2022-02-21 18:34:34 -0300

    Feat Sequelize: associate users

--- back-end/src/database/models/users.js
+++ back-end/src/database/models/users.js
@@ -1,10 +1,22 @@
 const user = (sequelize, DataTypes) => {
-  const users = sequelize.define('Users', {
+  const users = sequelize.define('user', {
     name: DataTypes.STRING,
     password: DataTypes.STRING,
     email: DataTypes.STRING,
     role: DataTypes.STRING,
-  })
+  },
+  {tableName: 'Users'});
+
+  users.associate = (models) => {
+    users.hasMany(models.Sales,
+      {
+        foreignKey: 'user_id',
+        otherKey: 'seller_id',
+        through: users,
+        as: 'sales',
+      }  
+    )
+  }
   return users;
 }
 

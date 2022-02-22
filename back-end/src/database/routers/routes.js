e5bd6ce3ec10a5a613e3b07932172197abdb5044
Antonio Malato <malatoneto@gmail.com>
2022-02-22 16:21:40 -0300

    Feat MSC: criando estrutura msc e add pasta utils e router

---
+++ back-end/src/database/routers/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/');
+router.get('/');
+router.update('/');
+router.delete('/');
+
+module.exports = {
+  router,
+}

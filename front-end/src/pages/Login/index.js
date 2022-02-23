f588a8cd453b0a29a9cfcee4691cbd0123002693
Heitor Santos <heitorg33@gmail.com>
2022-02-23 19:47:23 -0300

    Feat and Fix: fix func validateLogin and create button disable

--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -11,38 +11,60 @@ function Login() {
 
   const handleChange = (setState, event) => {
     setState(event.target.value);
-  }
+  };
+  const validateLogin = (callback, data, setState) => {
+    if (!callback(data)) {
+      setState(true);
+    } else {
+      setState(false);
+    }
+  };
 
-   return (
-    <form>
+  const handleClick = () => {
+    validateLogin(validate.validateEmail, emailInput, setIsVisibleErrorEmail);
+    validateLogin(
+      validate.validatePassword,
+      passwordInput,
+      setIsVisibleErrorPassword,
+    );
+  };
+
+  return (
+    <form className="form-login">
       <h1>Login</h1>
       <Input
         handleChange={ (event) => handleChange(setEmailInput, event) }
         dataTest="common_login__input-email"
-        value={emailInput}
+        value={ emailInput }
         id="email-input"
         placeholder="email"
         type="text"
       />
-
+      {isVisibleErrorEmail && (
+        <p data-testid="common_login__element-invalid-email">error</p>
+      )}
       <Input
         handleChange={ (event) => handleChange(setPasswordInput, event) }
         dataTest="common_login__input-password"
-        value={passwordInput}
+        value={ passwordInput }
         id="password-input"
         placeholder="password"
         type="password"
       />
-     
+      {isVisibleErrorPassword && <p>error</p>}
+
       <div>
         <Button
+          handleClick={ handleClick }
           dataTest="common_login__button-login"
           text="Login"
+          disabled={
+            !(
+              validate.validateEmail(emailInput)
+              && validate.validatePassword(passwordInput))
+          }
         />
-        <Button
-          dataTest="common_login__button-register"
-          text="Register"
-        />
+        <Button dataTest="common_login__button-register" text="Register" />
       </div>
     </form>
   );

b68cd056a3eb4fa9ad9ee59fbfcc39141f968ce0
Heitor Santos <heitorg33@gmail.com>
2022-02-23 16:58:21 -0300

    Feat: handle changes inputs

--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -1,15 +1,31 @@
 import React, { useState } from 'react';
 import * as validate from '../../utils/validate';
 import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 function Login() {
   const [emailInput, setEmailInput] = useState('');
   const [passwordInput, setPasswordInput] = useState('');
 
+  const handleEmail = (event) => {
+    if(validate.validateEmail(event.target.value)) {
+
+      setEmailInput(event.target.value)
+    }
+  }
+
+  const handlePassword = (event) => {
+    if(validate.validatePassword(event.target.value)) {
+
+      setPasswordInput(event.target.value)
+    }
+  }
+
   return (
     <form>
       <h1>Login</h1>
       <Input
+        handleChange={ handleEmail }
         dataTest="common_login__input-email"
         value={emailInput}
         id="email-input"
@@ -18,6 +34,7 @@ function Login() {
       />
 
       <Input
+        handleChange={ handlePassword }
         dataTest="common_login__input-password"
         value={passwordInput}
         id="password-input"
@@ -26,8 +43,14 @@ function Login() {
       />
      
       <div>
-        <button type="button" data-testid="common_login__button-login">Login</button>
-        <button type="button" data-testid="common_login__button-register">Register</button>
+        <Button
+          dataTest="common_login__button-login"
+          text="Login"
+        />
+        <Button
+          dataTest="common_login__button-register"
+          text="Register"
+        />
       </div>
     </form>
   );

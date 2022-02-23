6f5eb1819da8c7632faaa1140804404e4c79ca11
Heitor Santos <heitorg33@gmail.com>
2022-02-23 17:27:56 -0300

    Feat: add setState parameter

--- front-end/src/utils/validate.js
+++ front-end/src/utils/validate.js
@@ -1,12 +1,14 @@
-const validateEmail = (email) => {
+const validateEmail = (setState, email) => {
   const emailRegex = /\S+@\S+\.\S+/;
   const isValidEmail = emailRegex.test(email); 
    if(!isValidEmail) return false;
+   setState(true);
    return true;
 };
 
-const validatePassword = (password) => {
+const validatePassword = (setState, password) => {
   if(password.length < 6) return false;
+  setState(true);
   return true;
 };
 

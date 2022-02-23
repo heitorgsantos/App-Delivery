17589f1176ec46035893880408d4ff0c527c3f74
Heitor Santos <heitorg33@gmail.com>
2022-02-23 19:48:35 -0300

    Fix: func validate login and password

--- front-end/src/utils/validate.js
+++ front-end/src/utils/validate.js
@@ -1,17 +1,15 @@
-const validateEmail = (setState, email) => {
+const validateEmail = (email) => {
   const emailRegex = /\S+@\S+\.\S+/;
   const isValidEmail = emailRegex.test(email);
   if (!isValidEmail) {
-    setState(true);
     return false;
   }
   return true;
 };
 
-const validatePassword = (setState, password) => {
+const validatePassword = (password) => {
   const maxLength = 6;
   if (password.length < maxLength) {
-    setState(true);
     return false;
   }
   return true;

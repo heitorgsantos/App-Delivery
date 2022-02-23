a3664ab0e38718b5a5dbae1d1d76bae0b170fad0
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2022-02-23 15:08:50 -0300

    feat: cria elementos da tela de login

--- back-end/src/database/utils/joiValidations.js
+++ back-end/src/database/utils/joiValidations.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const schemaUser = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  password: Joi.string().min(5).required(),
+  password: Joi.string().min(6).required(),
   role: Joi.string().required(),
 });
 
--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <h1>Login</h1>
+      <div>
+      <input data-testid="common_login__input-email" type="text" placeholder="email" />
+      </div>
+      <div>
+      <input data-testid="common_login__input-password" type="password" placeholder="password" />        
+      </div>
+      <div>
+      <button data-testid="common_login__button-login">Login</button>        
+      <button data-testid="common_login__button-register">Register</button>        
+      </div>
+    </div>
   );
 }
 

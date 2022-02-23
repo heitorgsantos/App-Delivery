30e9e9e6eff115db39e7e0f33f7bbccb25554532
israeltonholo <israeltonholo@hotmail.com>
2022-02-23 17:07:30 -0300

    add routes

--- back-end/src/database/routers/routes.js
+++ back-end/src/database/routers/routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const createUserController = require('../controllers/users/createUserController');
+const { createUserController, loginUserController } = require('../controllers/users');
 
 router.post('/register', createUserController);
+router.post('/login', loginUserController);
 // router.get('/');
 // router.update('/');
 // router.delete('/');

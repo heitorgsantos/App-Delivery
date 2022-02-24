d334d4a0d407882c410cf0a5159197e94c6e306c
Rodrigo <rodrigoaugusto96@outlook.com>
2022-02-24 16:40:32 -0300

    Feat: Add route to register

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home/index';
+import Register from './pages/Register/index';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Home } />
       <Route exact path="/login" component={ Login } />
+      <Route exact path="/register" component={ Register } />
     </Switch>
   );
 }

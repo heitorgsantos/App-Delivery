92e3672c73747f3e9a8ebbd6567374ff578b56e4
Rodrigo <rodrigoaugusto96@outlook.com>
2022-02-23 21:41:51 -0300

    Feat: Use axios and create validation to hash

--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -1,32 +1,41 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import * as validate from '../../utils/validate';
+import * as axios from '../../utils/axios';
+import * as md5 from '../../utils/md5';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 function Login() {
+  const history = useHistory();
   const [emailInput, setEmailInput] = useState('');
   const [passwordInput, setPasswordInput] = useState('');
   const [isVisibleErrorEmail, setIsVisibleErrorEmail] = useState(false);
-  const [isVisibleErrorPassword, setIsVisibleErrorPassword] = useState(false);
 
   const handleChange = (setState, event) => {
     setState(event.target.value);
   };
   const validateLogin = (callback, data, setState) => {
-    if (!callback(data)) {
+    if (callback(data)) {
       setState(true);
     } else {
       setState(false);
     }
   };
 
-  const handleClick = () => {
-    validateLogin(validate.validateEmail, emailInput, setIsVisibleErrorEmail);
-    validateLogin(
-      validate.validatePassword,
-      passwordInput,
-      setIsVisibleErrorPassword,
-    );
+  const handleClick = async () => {
+    try {
+      const encryptedPassword = md5.encryptPassword(passwordInput);
+      const { data } = await axios.postLoginData({
+        email: emailInput, password: passwordInput });
+      if (encryptedPassword === data.valid) {
+        history.push('/customer/products');
+      }
+    } catch (error) {
+      if (error.message.includes('404')) {
+        validateLogin(validate.validateEmail, emailInput, setIsVisibleErrorEmail);
+      }
+    }
   };
 
   return (
@@ -41,7 +50,11 @@ function Login() {
         type="text"
       />
       {isVisibleErrorEmail && (
-        <p data-testid="common_login__element-invalid-email">error</p>
+        <p
+          data-testid="common_login__element-invalid-email"
+        >
+          Erro: Email não cadastrado
+        </p>
       )}
       <Input
         handleChange={ (event) => handleChange(setPasswordInput, event) }
@@ -51,7 +64,6 @@ function Login() {
         placeholder="password"
         type="password"
       />
-      {isVisibleErrorPassword && <p>error</p>}
 
       <div>
         <Button

924157ac7a5b7dd5ef6deb56243590f4727325d5
Rodrigo <rodrigoaugusto96@outlook.com>
2022-02-24 07:50:24 -0300

    Fix: change import/export to es6

--- front-end/src/utils/axios.js
+++ front-end/src/utils/axios.js
@@ -1,11 +1,9 @@
-const axios = require('axios');
+import axios from 'axios';
 
-const postLoginData = async (data) => {
+export default postLoginData = async (data) => {
   const URL = 'http://localhost:3001/login';
   const response = await axios.post(URL, data, {
     mode: 'no-cors',
   });
   return response;
 };
-
-module.exports = { postLoginData };

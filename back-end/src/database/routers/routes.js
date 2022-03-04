bc03e4580d162c136166f1cf38f0b021aa95d7b4
Heitor Santos <heitorg33@gmail.com>
2022-03-04 18:09:44 -0300

    Feat: create router update status sales

--- back-end/src/database/routers/routes.js
+++ back-end/src/database/routers/routes.js
@@ -6,6 +6,7 @@ const findAllController = require('../controllers/users/findUserController');
 const createSalesController = require('../controllers/sales/createSalesController');
 const findSalesSellerById = require('../controllers/sales/findSalesSellerController');
 const findSalesByClientId = require('../controllers/sales/findSalesByClientId ');
+const updateStatusSales = require('../controllers/sales/updateStatusSalesController');
 
 router.post('/register', createUserController);
 router.post('/login', loginUserController);
@@ -15,6 +16,8 @@ router.get('/login', findAllController);
 router.get('/register', findAllController);
 router.get('/seller/orders/:id', findSalesSellerById);
 router.get('/customer/orders/:id', findSalesByClientId);
+
+router.put('/seller/orders/:id', updateStatusSales);
 
 module.exports = {
   router,

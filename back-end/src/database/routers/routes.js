0656189b9e43c0453d1e3713f827ecb45a67fae0
Heitor Santos <heitorg33@gmail.com>
2022-03-04 14:58:49 -0300

    Feat: create router GET find seller to ID

--- back-end/src/database/routers/routes.js
+++ back-end/src/database/routers/routes.js
@@ -4,12 +4,14 @@ const createUserController = require('../controllers/users/createUserController'
 const loginUserController = require('../controllers/users/loginUserController');
 const findAllController = require('../controllers/users/findUserController');
 const createSalesController = require('../controllers/sales/createSalesController');
+const findSalesSellerById = require('../controllers/sales/findSalesSallerController');
 
 router.post('/register', createUserController);
 router.post('/login', loginUserController);
 
 router.get('/login', findAllController);
 router.get('/register', findAllController);
+router.get('/id', findSalesSellerById);
 // router.update('/');
 // router.delete('/');
 

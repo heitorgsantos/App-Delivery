92a661c5c2807680d2a3afa027069582fbd4f9ab
Israel <israeltonholo@hotmail.com>
2022-03-04 16:28:58 -0300

    fix: corrige rota getSalesBySellerId

--- back-end/src/database/routers/routes.js
+++ back-end/src/database/routers/routes.js
@@ -4,7 +4,7 @@ const createUserController = require('../controllers/users/createUserController'
 const loginUserController = require('../controllers/users/loginUserController');
 const findAllController = require('../controllers/users/findUserController');
 const createSalesController = require('../controllers/sales/createSalesController');
-const findSalesSellerById = require('../controllers/sales/createSalesController');
+const findSalesSellerById = require('../controllers/sales/findSalesSellerController');
 const findSalesByClientId = require('../controllers/sales/findSalesByClientId ');
 
 router.post('/register', createUserController);
--- back-end/src/database/services/sales/findSalesSellerService.js
+++ back-end/src/database/services/sales/findSalesSellerService.js
@@ -2,9 +2,7 @@ const { sale } = require('../../models/index');
 const constructorError = require('../../utils/constructorError');
 
 const findSalesSellerById = async (seller_id) => {
-  console.log(seller_id);
-  const findSalesSeller = await sale.findOne({ where: { id: seller_id }});
-  console.log(findSalesSeller);
+  const findSalesSeller = await sale.findAll({ where: { seller_id: seller_id }});
 
   if (!findSalesSeller) throw constructorError(404, 'Seller not found');
   return findSalesSeller;

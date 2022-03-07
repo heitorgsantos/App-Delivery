a38dd950955f320704581d6b9899705c671e7d82
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2022-03-07 16:18:52 -0300

    fix: inclui parametro no toLocaleDateString('pt-br')

--- back-end/src/database/services/sales/findSalesByCustomerId.js
+++ back-end/src/database/services/sales/findSalesByCustomerId.js
@@ -2,7 +2,7 @@ const { sale } = require('../../models/index');
 const constructorError = require('../../utils/constructorError');
 
 const findSalesByCustomerId = async (id) => {
-  const find = await sale.findAll({ where: { user_id: id } });
+  const find = await sale.findAll({ where: { id } });
   if (!find) throw constructorError(404, 'Invalid Id, try again!');
 
   return find

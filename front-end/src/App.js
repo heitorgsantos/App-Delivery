63202287102dbec93fcb59e589a85f3114e386a3
Rodrigo <rodrigoaugusto96@outlook.com>
2022-03-07 17:47:19 -0300

    Feat: Add route to page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,6 +8,7 @@ import Products from './pages/Customer';
 import Checkout from './pages/Checkout';
 import OrderDetails from './pages/OrderDetails';
 import Orders from './pages/CustomerOrders';
+import OrderSellerDetails from './pages/OrderSellerDetails';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       <Route exact path="/customer/checkout" component={ Checkout } />
       <Route exact path="/customer/orders" component={ Orders } />
       <Route exact path="/customer/orders/:id" component={ OrderDetails } />
+      <Route exact path="/seller/orders/:id" component={ OrderSellerDetails } />
     </Switch>
   );
 }

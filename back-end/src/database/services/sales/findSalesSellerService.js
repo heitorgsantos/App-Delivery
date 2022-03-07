78d2e02877377880f035f66875fa503ed0133177
Israel <israeltonholo@hotmail.com>
2022-03-07 20:33:43 -0300

    fix: adiciona salesProducts ao findAll

--- back-end/src/database/services/sales/findSalesSellerService.js
+++ back-end/src/database/services/sales/findSalesSellerService.js
@@ -1,8 +1,17 @@
-const { sale } = require('../../models/index');
+const { sale, salesProduct } = require('../../models/index');
 const constructorError = require('../../utils/constructorError');
 
 const findSalesSellerById = async (seller_id) => {
-  const findSalesSeller = await sale.findAll({ where: { seller_id: seller_id }});
+  const findSalesSeller = await sale.findAll({ 
+    where: { seller_id: seller_id }, 
+    include: [
+      {
+        model: salesProduct,
+        as: 'products',
+        attributes: {exclude: ['createdAt', 'updatedAt']}
+      }
+    ]
+  });
   if (!findSalesSeller) throw constructorError(404, 'Seller not found');
   return findSalesSeller;
 }
